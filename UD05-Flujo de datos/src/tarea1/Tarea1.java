dea5e0ed40cb7abc03b042d9439b732c37979b0f
Chals <csanchezbasket@gmail.com>
2022-10-03 10:10:49 +0200

    Commit Tarea 1

---
+++ UD05-Flujo de datos/src/tarea1/Tarea1.java
@@ -0,0 +1,18 @@
+package tarea1;
+
+public class Tarea1 {
+
+	public static void main(String[] args) {
+
+		int a = 40;
+		int b = 4;
+		if(a < b)
+			System.out.println(a + " es menor a " + b);
+		if (a > b)
+			System.out.println(a + " es mayor a " + b);
+		if(a == b)
+			System.out.println(a + " es igual a " + b);
+
+	}
+
+}
